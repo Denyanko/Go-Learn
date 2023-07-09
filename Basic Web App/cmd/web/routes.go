051436d6caea278ec2fec66cd25ba4890723766f
Ade Widyatama Dian Boernama <adewidyatamadb@gmail.com>
2023-07-09 21:05:14 +0800

    using the real chi-router

---
+++ Basic Web App/cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"Basic_Web_App/pkg/config"
+	"Basic_Web_App/pkg/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
--- Basic Web App/go.mod
+++ Basic Web App/go.mod
@@ -1,3 +1,5 @@
 module Basic_Web_App
 
 go 1.20
+
+require github.com/go-chi/chi v1.5.4 // indirect
